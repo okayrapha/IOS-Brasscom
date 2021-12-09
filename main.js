4d2ae76711a1db478735d51daaf0eec3c0a41505
okayrapha <raphaelaa.nascimento@gmail.com>
2021-12-09 19:00:54 -0300

    Estruturas condicionais

---
+++ main.js
@@ -0,0 +1,44 @@
+// Estrutura condicional simples
+// Igual duplo
+const  a  =  11 ;
+if  ( a  ==  10 )
+{
+    console . log ( 'os valores são iguais' ) ;
+}
+    outro
+    {
+        console . log ( "Os valores são diferentes" ) ;
+    }
+
+console . limpar ( ) ;
+
+if  ( a  ==  10 )
+{
+    console . log ( 'os valores são iguais' ) ;
+}
+    else  if  ( a  >  10 )
+    {
+        console . log ( "A é maior que 10" ) ;
+    }
+        outro
+        {
+            console . log ( "A variável a não é igual a 10 e não é maior que 10" ) ;
+        }
+
+//TROCAR
+console . limpar ( ) ;
+
+const  nota  =  6 ; 0
+
+interruptor  ( nota )
+    {
+        caso  4 :
+            console . log ( "Reprovado" ) ;
+            pausa ;
+        caso  6 :
+            console . log ( "Recuperação" ) ;
+            pausa ;
+        padrão :
+            console . log ( "Nota superior a média, parabens!" ) ;
+            pausa ;
+    }
